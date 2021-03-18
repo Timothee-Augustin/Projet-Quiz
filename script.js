c6c454539c28ed19e69232c9812ec20b820493dd
Timothee <augustin.timothee@gmail.com>
2021-03-18 11:57:37 +0100

    added answer selection

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 const rulesButton = document.getElementById("rules");
+const answerButton = document.getElementsByClassName("answer");
+let answerChoice = null;
+let correctAnswer = 0;
+//const validButton = document.getElementById("validation");
 
 rulesButton.addEventListener("click", function(){
-    alert("-le joueur commence avec une cagnotte à 0 \n-en cas de bonne réponse gain de wildCoins \n-en cas de mauvais réponse perte de wildCoins");
+    alert("- Vous commencez avec une cagnotte à 0 \n- En cas de bonne réponse vous gagner des wildCoins \n- Mais en cas de mauvais réponse vous perdez des wildCoins");
+});
+
+
+for (let i = 0; i < answerButton.length; i++){
+   
+    answerButton[i].addEventListener("click", function(){
+        answerButton[i].style.background = "yellow";
+        answerChoice = i;
+        for (let j = 0; j < i; j++){
+            answerButton[j].style.background = "white";
+        }
+        for (let j = i+1; j < answerButton.length; j++){
+            answerButton[j].style.background = "white";
+        }        
+        }) 
+
+};
+    
+/**validButton.addEventListener("click", function(){
+    if (answerChoice === correctAnswer) {
+        alert("coorect")
+    } 
+    else {
+        alert("faux");
+    }
 })
+*/
