b1da44ad2eae477f8af29408727ffefe6bea85b7
BrianBTI <bertili.brian@gmail.com>
2021-03-18 11:53:52 +0100

    CSS work and button validation animation

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const rulesButton = document.getElementById("rules");
 rulesButton.addEventListener("click", function(){
     alert("-le joueur commence avec une cagnotte à 0 \n-en cas de bonne réponse gain de wildCoins \n-en cas de mauvais réponse perte de wildCoins");
 })
+
+/** const validButton = document.getElementById("validation");
+
+validButton.addEventListener('mouseover', function(){
+    validButton.innerHTML = "C'est votre ultime bafouille ?";
+    validButton.style.backgroundColor = "orange";
+    validButton.style.color = "black"
+    validButton.addEventListener('mouseleave', function() {
+        validButton.innerHTML = "Valider";
+        validButton.style.backgroundColor = "";
+        validButton.style.color = ""
+});
+});
+*/
+
+
