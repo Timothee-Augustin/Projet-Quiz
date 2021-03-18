8f07d5a444fb3763aeee84d0684e90f18ebe4f43
cyrildav <cyril.david51@hotmail.fr>
2021-03-18 11:52:56 +0100

    background color

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const rulesButton = document.getElementById("rules");
 rulesButton.addEventListener("click", function(){
     alert("-le joueur commence avec une cagnotte à 0 \n-en cas de bonne réponse gain de wildCoins \n-en cas de mauvais réponse perte de wildCoins");
 })
+
